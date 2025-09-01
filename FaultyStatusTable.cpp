52a7a19d54d7309c307f391ce11142dbcf54865f
tianchong <1980326508@qq.com>
2025-09-01 17:08:58 +0800

    继续修改

--- FaultyStatusTable.cpp
+++ FaultyStatusTable.cpp
@@ -1,12 +1,7 @@
 #include "FaultyStatusTable.h"
-#include "UniversalDelegate.h"
+#include "FaultyTableDelegate.h"
 
 FaultyStatusTable::FaultyStatusTable(QWidget* parent) : BaseDataTable(parent)
 {
-    auto* delegate = new UniversalDelegate(this);
-
-    // 关键配置：告诉委托，第3列（索引为2）需要被绘制成状态圆点。
-    delegate->setStatusCircleColumn(2);
-
-    setItemDelegate(delegate);
+    setItemDelegate(new FaultyTableDelegate(this));
 }
